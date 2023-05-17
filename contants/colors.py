a1cfa1948cf8473af70ac9151a4bb9d1beab32c1
Meta <john.wesley.scott@protonmail.com>
2023-05-16 19:25:37 -0500

    use poetry

---
+++ contants/colors.py
@@ -0,0 +1,12 @@
+pairs = {
+    'white_on_black': (
+        (255, 255, 255),
+        (0, 0, 0)
+    ).
+    'red_on_black': (
+        (255, 0, 0),
+        (0, 0, 0)
+    ).
+}
+
+print(pairs['white_on_black'])
