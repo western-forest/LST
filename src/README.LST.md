51716be44ce8ebe162c9d8c092d9c925e0cc69b3
Meta <john.wesley.scott@protonmail.com>
2023-05-27 13:46:47 -0500

    folder refactor

---
+++ src/README.LST.md
@@ -0,0 +1,10 @@
+# L.S.T.
+
+L.S.T. lists with elegant command line interface 
+
+## important features
+
+* intuitive beautiful retro future renegade command line interface
+* multiple stunning color schemes
+* export boards and lists as JSON, TOML, or CSV
+* kanban boards too! so you can agile with one lovely little program 
---
+++ src/contants/text.py
@@ -0,0 +1,47 @@
+typing_prompts = (
+    'the them then they there their these this those though three through that '
+    'them thank think thing thought throne threw ten tell till time to too '
+    'told toward two twice take took true truth try tried turn',
+    
+    'we week well went when what where whether which while who whole why was '
+    'way will with wish work worth world worse worst would woman women',
+
+    'an and any are arrive around as ask assure at about able act across '
+    'account after ago again all allude allow almost also already always alone '
+    'am among amount aqdd admit apart appear apply awqay answer arrange after '
+    'affect already always alone am among amount add admit apart appear apply',    '
+)
+'∫∫'
+
+ui = {
+    'opening_title':  'Typing Test ~OR~ Learn Curses + Refresh Python!'
+    'sports_commentary': {
+        'bad': (
+            '</3'
+            'D:'
+            'let go!',
+            'not good ~ you will get it! KEEP TRYiNG!',
+            'meh',
+            'not great...',
+            'nonsense!'',
+            'oops!',
+            'ouch :/',
+            'reach out with your feelings'
+            'try...?!'
+        ),
+        'good': (
+            ':D',
+            '<3',
+            'c:',
+            'damn, me ho',
+            'damn, son',
+            'hope!',
+            'love yourself',
+            'otlitchna!'
+            'search!',
+            'typing is fundee-mental',
+            'wow wo we wa!',
+            'wow you are fast!'
+        )
+    }
+}
